4f84afbfaec2f56f274a2276726b95753e334c26
The Connect <121242664+2678Th@users.noreply.github.com>
2025-10-13 06:55:22 -0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,15 +1,15 @@
 // Automated header background image slider for Home page
 const images = [
-    '../img/african-social-worker-helping-senior-woman.jpg',
-    '../img/book-wall-1151405_640.jpg',
-    '../img/dementia-8734695_640.jpg',
-    '../img/full-shot-senior-couple-with-cat-home.jpg',
-    '../img/fun-activities-in-old-age-home.png',
-    '../img/house-217260_640.jpg',
-    '../img/nurse-taking-care-elderly-patient.jpg',
-    '../img/OIP.webp',
-    '../img/Old-Age-Home-Delhi.jpg',
-    '../img/portrait-middle-aged-businesswoman.jpg'
+    'img/african-social-worker-helping-senior-woman.jpg',
+    'img/book-wall-1151405_640.jpg',
+    'img/dementia-8734695_640.jpg',
+    'img/full-shot-senior-couple-with-cat-home.jpg',
+    'img/fun-activities-in-old-age-home.png',
+    'img/house-217260_640.jpg',
+    'img/nurse-taking-care-elderly-patient.jpg',
+    'img/OIP.webp',
+    'img/Old-Age-Home-Delhi.jpg',
+    'img/portrait-middle-aged-businesswoman.jpg'
 ];
 let current = 0;
 const headerBg = document.getElementById('header-bg');
@@ -24,3 +24,4 @@ setInterval(() => {
     current = (current + 1) % images.length;
     setHeaderBg();
 }, 3500);
+
